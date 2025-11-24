b832654454e37360b8d943db034f234e3fbef9b4
UltralyticsAssistant <web@ultralytics.com>
2025-11-24 04:44:47 +0000

    Auto-format by https://ultralytics.com/actions

--- emailService.py
+++ emailService.py
@@ -1,11 +1,12 @@
 import smtplib
-from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 # Gmail login details
 sender_email = "0863ad231028@gmail.com"
-password = "eejq uenu stbt eafp"   # Gmail App Password
+password = "eejq uenu stbt eafp"  # Gmail App Password
 receiver_email = "aagamjain2704@gmail.com"
+
 
 def SendEmail(subject="⚠ Alert: Person Count Notification", body="Person count has reached 10!"):
     msg = MIMEMultipart()
